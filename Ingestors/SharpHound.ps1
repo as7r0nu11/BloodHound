2811f5f24da871f6ec76d565c0a167715860f9ba
as7r0nu11 <56869349+as7r0nu11@users.noreply.github.com>
2020-04-06 17:00:33 +0300

    Update SharpHound.ps1

--- Ingestors/SharpHound.ps1
+++ Ingestors/SharpHound.ps1
@@ -510,4 +510,4 @@ function Invoke-BloodHound{
 	$Assembly.GetType("Costura.AssemblyLoader", $false).GetMethod("Attach", $BindingFlags).Invoke($Null, @())
 	$Assembly.GetType("SharpHound3.SharpHound").GetMethod("InvokeSharpHound").Invoke($Null, @(,$passed))
 }
-Invoke-BloodHound -Domain prod.mobily.lan -CollectionMethod All -Throttle 10000 -Jitter 50
+Invoke-BloodHound -Domain prod.mobily.lan -CollectionMethod Group
